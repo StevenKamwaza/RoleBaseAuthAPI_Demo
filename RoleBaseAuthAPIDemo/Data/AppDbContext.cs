6cc60a5a750b952a867ff63589bcf035c7ca17a3
Steven Kamwaza <48084787+StevenKamwaza@users.noreply.github.com>
2024-09-09 13:33:17 +0200

    Update AppDbContext.cs

--- RoleBaseAuthAPIDemo/Data/AppDbContext.cs
+++ RoleBaseAuthAPIDemo/Data/AppDbContext.cs
@@ -2,14 +2,12 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RoleBaseAuthAPIDemo.Domain.Models;
-using System.Diagnostics.Metrics;
 
 namespace RoleBaseAuthAPIDemo.Data
 {
     public class AppDbContext: IdentityDbContext<IdentityUser>
     {
 
-      
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
