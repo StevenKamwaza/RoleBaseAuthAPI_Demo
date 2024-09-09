b6186b2f01beb88fb6df8e90aac0548182219dd5
Steven Kamwaza <48084787+StevenKamwaza@users.noreply.github.com>
2024-09-09 07:59:50 +0200

    updates

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RoleBaseAuthAPIDemo.Domain.Models;
+using System.Diagnostics.Metrics;
+
+namespace RoleBaseAuthAPIDemo.Infrastructure.Data
+{
+    public class AppDbContext: IdentityDbContext<IdentityUser>
+    {
+
+      
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
